9c99fdb6132e960eeb9836c55f72c7080da14559
ishqdehlvi <inzemamhaq05@gmail.com>
2023-07-29 21:43:26 +0530

    pages redesigned

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("js is working");
+// if the navbar of bootstrap is not working,
+// create a function that gets invoked when media query hits 
+// the function will remove all content of navbar and give a single button
+// that button when clicked will display the navbar with a transition to scale the div vertically and show the links and buttons 
